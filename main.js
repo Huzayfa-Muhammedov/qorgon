66203ffe647ed2fca30fb09da7d81d28a086bbc0
Пользователь <zubayrm845@gmail.com>
2025-02-28 18:18:56 +0500

    add HTML file tegst

---
+++ main.js
@@ -0,0 +1,5 @@
+var userAge = Number (prompt ("Yoshingizni Kiriting "));
+
+alert(`Sizning tugilgan kuningiz: ${2025 - userAge}`);
+
+alert(`Siz keyingi yil ${userAge + 1} yoshda bolasiz ana o'shanda pasport olasiz`)
